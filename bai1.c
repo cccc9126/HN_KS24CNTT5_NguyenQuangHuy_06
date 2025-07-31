5db92e4f52a6781557b4233d24dcf1d7e800b46a
Cachiu <cachiu@Cachius-MacBook-Air.local>
2025-07-31 15:28:45 +0700

    first commit

---
+++ bai1.c
@@ -0,0 +1,215 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAX_LEN 100
+
+typedef struct Node {
+    char bookTitle[MAX_LEN];
+    struct Node *next;
+} Node;
+
+typedef struct Stack {
+    Node *top;
+} Stack;
+
+typedef struct Queue {
+    Node *front;
+    Node *rear;
+} Queue;
+
+void initStack(Stack *s) {
+    s->top = NULL;
+}
+
+int isEmpty(Stack *s) {
+    return s->top == NULL;
+}
+
+void push(Stack *s, char *bookTitle) {
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    strcpy(newNode->bookTitle, bookTitle);
+    newNode->next = s->top;
+    s->top = newNode;
+}
+char* pop(Stack *s) {
+    if (isEmpty(s)) {
+        printf("Stack is empty\n");
+        return NULL;
+    }
+    Node *temp = s->top;
+    char* bookTitle = strdup(temp->bookTitle);
+    s->top = s->top->next;
+    free(temp);
+    return bookTitle;
+}
+
+void printStack(Stack *s) {
+    Node *temp = s->top;
+    while (temp != NULL) {
+        printf("%s\n", temp->bookTitle);
+        temp = temp->next;
+    }
+}
+
+void initQueue(Queue *q) {
+    q->front = NULL;
+    q->rear = NULL;
+}
+
+int isEmptyQueue(Queue *q) {
+    return q->front == NULL;
+}
+
+void enqueue(Queue *q, char *bookTitle) {
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    strcpy(newNode->bookTitle, bookTitle);
+    newNode->next = NULL;
+    if (isEmptyQueue(q)) {
+        q->front = newNode;
+        q->rear = newNode;
+    } else {
+        q->rear->next = newNode;
+        q->rear = newNode;
+    }
+}
+
+char* dequeue(Queue *q) {
+    if (isEmptyQueue(q)) {
+        printf("Queue is empty\n");
+        return NULL;
+    }
+    Node *temp = q->front;
+    q->front = q->front->next;
+    if (q->front == NULL) {
+        q->rear = NULL;
+    }
+    char* bookTitle = strdup(temp->bookTitle);
+    free(temp);
+    return bookTitle;
+}
+
+void printQueue(Queue *q) {
+    Node *temp = q->front;
+    while (temp != NULL) {
+        printf("%s\n", temp->bookTitle);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+
+void freeQueue(Queue *q) {
+    while (!isEmptyQueue(q)) {
+        dequeue(q);
+    }
+}
+
+
+void freeStack(Stack *s) {
+    while (!isEmpty(s)) {
+        pop(s);
+    }
+}
+
+
+int main() {
+    Stack *bookStack = (Stack *) malloc(sizeof(Stack));
+    Queue *closedBook = (Queue *) malloc(sizeof(Queue));
+    initStack(bookStack);
+    initQueue(closedBook);
+    int choice;
+    char current[MAX_LEN];
+
+    do {
+        printf("==========E-BOOK READER MANAGER==========\n");
+        printf("1. Mở sách mới\n");
+        printf("2. Đóng sách hiên tại\n");
+        printf("3. Xem sách đang đọc gần nhất\n");
+        printf("4. Lịch sử đóng sách\n");
+        printf("5. Khôi phục sách\n");
+        printf("6. Hiển thị các sách đang mở\n");
+        printf("7. Thoát\n");
+        printf("========================================\n");
+        printf("Mời nhập lựa chọn của bạn: ");
+        scanf("%d", &choice);
+        getchar();
+        switch (choice) {
+            case 1:
+                char newBook[MAX_LEN];
+                printf("Nhập tên sách mới: ");
+                fgets(newBook, MAX_LEN, stdin);
+                newBook[strcspn(newBook, "\n")] = '\0';
+                push(bookStack, newBook);
+
+                printStack(bookStack);
+                break;
+
+            case 2:
+                if (!isEmpty(bookStack)) {
+                    char* current = pop(bookStack);
+                    if (current != NULL) {
+                        enqueue(closedBook, current);
+                        free(current);
+                        printf("Sách đã được đóng và chuyển vào lịch sử\n");
+
+                    }
+                } else {
+                    printf("Không có sách nào đang mở\n");
+                }
+                break;
+
+            case 3:
+                if (!isEmpty(bookStack)) {
+                    printf("Sách đang đọc gần nhất: %s\n", bookStack->top->bookTitle);
+                } else {
+                    printf("Không có sách nào đang mở\n");
+                }
+                break;
+
+            case 4:
+                if (!isEmptyQueue(closedBook)) {
+                    printf("Lịch sử sách đã đóng:\n");
+                    printQueue(closedBook);
+                } else {
+                    printf("Chưa có sách nào trong lịch sử\n");
+                }
+                break;
+case 5:
+                if (!isEmptyQueue(closedBook)) {
+                    char*temp = dequeue(closedBook);
+                    printf("%s", temp);
+                    push(bookStack, temp);
+                    free(temp);
+
+
+
+
+                }else {
+                    printf("Không có sách nào đang đóng.\n");
+                }
+                break;
+            case 6:
+                if (!isEmpty(bookStack)) {
+                    printf("Các sách đang mở:\n");
+                    printStack(bookStack);
+                } else {
+                    printf("Không có sách nào đang mở\n");
+                }
+                break;
+            case 7:
+
+                freeStack(bookStack);
+                freeQueue(closedBook);
+                break;
+            default:
+                printf("Lựa chọn không hợp lệ .");
+                break;
+        }
+    } while (choice != 7);
+printf("Thoát chương trình....\n");
+    return 0;
+}
+
+
+
---
+++ bai2.c
@@ -0,0 +1,153 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_LEN 200
+
+typedef struct Node {
+    char content[MAX_LEN];
+    int isAction;
+    struct Node *yesChild;
+    struct Node *noChild;
+} Node;
+
+Node* create(const char* data) {
+    Node *newNode = (Node *) malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+    strncpy(newNode->content, data, MAX_LEN - 1);
+    newNode->content[MAX_LEN - 1] = '\0';
+    newNode->isAction = 0;
+    newNode->yesChild = NULL;
+    newNode->noChild = NULL;
+    return newNode;
+}
+
+Node* addTree(Node *root, const char* data) {
+    if (root == NULL) {
+        return create(data);
+    }
+
+    printf("Nhánh:  %s\n", root->content);
+    printf(" 1: Có | 2: Không : ");
+    int choice;
+    scanf("%d", &choice);
+    getchar();
+
+    if (choice == 1) {
+        if (root->yesChild == NULL) {
+            root->yesChild = create(data);
+        } else {
+            root->yesChild = addTree(root->yesChild, data);
+        }
+    } else if (choice == 2) {
+        if (root->noChild == NULL) {
+            root->noChild = create(data);
+        } else {
+            root->noChild = addTree(root->noChild, data);
+        }
+    }
+
+    return root;
+}
+
+void display(Node *root) {
+    if (root == NULL) return;
+    printf("%s\n",root->content);
+    display(root->yesChild);
+    display(root->noChild);
+}
+
+void playGame(Node *root) {
+    if (root == NULL) return;
+
+    printf("\n%s (yes/no): ", root->content);
+    char answer[10];
+    fgets(answer, sizeof(answer), stdin);
+    answer[strcspn(answer, "\n")] = 0;
+
+    if (strcmp(answer, "yes") == 0) {
+        if (root->yesChild!=NULL) {
+            playGame(root->yesChild);
+        } else {
+            printf("Kết thúc!\n");
+        }
+    } else if (strcmp(answer, "no") == 0) {
+        if (root->noChild!=NULL) {
+            playGame(root->noChild);
+        } else {
+            printf("Kết thúc!\n");
+        }
+    } else {
+        printf("Nhập 'yes' hoặc 'no'\n");
+        playGame(root);
+    }
+}
+
+void freeTree(Node *root) {
+    if (root == NULL) return;
+    freeTree(root->yesChild);
+    freeTree(root->noChild);
+    free(root);
+}
+Node* deleteNode(Node *root, const char* data) {
+    if (root == NULL) return NULL;
+    if (strcmp(root->content, data) == 0) {
+        Node *temp = root->yesChild;
+    }
+}
+int main() {
+    int choice;
+    Node *root = NULL;
+    char input[MAX_LEN];
+
+    do {
+        printf("\n=======GAME DECISION SIMULATOR=======\n");
+        printf("1. Bắt đầu\n");
+        printf("2. Hiện thị cây quyết định\n");
+        printf("3. Thêm hành động hoặc câu hỏi\n");
+        printf("4. Xóa 1 nút bất kì\n");
+        printf("5. Thoát\n");
+        printf("=====================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        getchar();
+
+        switch (choice) {
+            case 1:
+                if (root == NULL) {
+                    printf("Hãy thêm câu hỏi!\n");
+                } else {
+                    playGame(root);
+                }
+                break;
+            case 2:
+                if (root == NULL) {
+                    printf("Không có câu hỏi nào!\n");
+                } else {
+                    display(root);
+                }
+                break;
+            case 3:
+                printf("Nhập câu hỏi hoặc hành động: ");
+                fgets(input, MAX_LEN, stdin);
+                input[strcspn(input, "\n")] = 0;
+                root = addTree(root, input);
+                printf("Thêm thành công!\n");
+                break;
+            case 4:
+
+            case 5:
+                freeTree(root);
+                printf("Đang thoát chương tình!\n");
+                break;
+            default:
+                printf("Lựa chọn không hợp lệ.\n");
+                break;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
